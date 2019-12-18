08058d746ae8d33bb9023796cb4d75ffaba116d5
Baruch Phillips <baruchphillips@gmail.com>
2019-12-18 15:15:52 -0500

    Baruchs mod2 challenge

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,18 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show 
+    @heroine = Heroine.find(params[:id])
+  end 
+
+  def new 
+    @heroine = Heroine.new
+  end 
+
+  def create 
+    @heroine = Heroine.create(name: params[:heroine][:name],super_name: params[:heroine][:super_name],
+    power_id: params[:heroine][:power_id])
+    redirect_to heroines_path
+  end 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ powers = [
 ]
 
 powers.each {|power| Power.create(power)}
-
+# power_id in heroine
 heroines = [
   {name: "Kamala Khan", super_name: "Ms. Marvel"},
   {name: "Doreen Green", super_name: "Squirrel Girl" },
@@ -30,7 +30,7 @@ heroines = [
 # Just uncomment line 33 and run `rake db:seed` again in your terminal.
 # (******Note****** If you try to do this before you set up proper associations, it will error.)
 # ---------------
-heroines = heroines.map { |heroine| heroine.merge( { power_id: Power.all.sample.id } ) }
+ heroines = heroines.map { |heroine| heroine.merge( { power_id: Power.all.sample.id } ) }
 # ---------------
 
 heroines.each { |heroine| Heroine.create(heroine) }
